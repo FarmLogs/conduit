be59a9527854a1ef205c2bc8e8609a36c7f3934c
Brian Rowe <brianrowe@farmlogs.com>
2015-11-20 12:35:58 -0500

    Implement a connection component

---
+++ src/com/farmlogs/messaging/connection.clj
@@ -0,0 +1,21 @@
+(ns com.farmlogs.messaging.connection
+  (:require [com.stuartsierra.component :as component]
+            [clojure.tools.logging :as log]
+            [langohr.core :as rmq]))
+
+(defrecord RMQConnection
+    [conn uri]
+  component/Lifecycle
+  (start [this]
+    (log/info "Starting RMQ")
+    (assoc this
+           :conn (rmq/connect {:uri uri :automatically-recover true})))
+
+  (stop [this]
+    (log/info "Stopping RMQ")
+    (rmq/close conn)
+    (log/info "Stopped RMQ")))
+
+(defn connection
+  [uri]
+  (->RMQConnection nil uri))
