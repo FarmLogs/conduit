8661cdefb1d4eb6e723ddddebdb2e5878edce1bd
Brian Rowe <brianrowe@farmlogs.com>
2015-11-20 19:09:02 -0500

    Move logging dependencies to :dev profile

--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
                  [com.novemberain/langohr "3.4.1"]
                  [com.stuartsierra/component "0.3.0"]
                  [org.clojure/core.async "0.2.374"]
+                 [org.clojure/tools.logging "0.3.1"]]
 
-                 [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.7"]
-                 [org.slf4j/jcl-over-slf4j "1.7.7"]
-                 [org.slf4j/log4j-over-slf4j "1.7.7"]]
-
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+  :profiles {:dev {:dependencies
+                   [[org.clojure/test.check "0.9.0"]
+                    [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
+                    [org.slf4j/jul-to-slf4j "1.7.7"]
+                    [org.slf4j/jcl-over-slf4j "1.7.7"]
+                    [org.slf4j/log4j-over-slf4j "1.7.7"]]}})
